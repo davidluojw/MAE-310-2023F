e227504067d059a6e94698a77fc35a1ec3ffc9cc
lawkawai <ddlljiawei@163.com>
2023-12-04 22:33:26 +0800

    create L, Lp array and y0 and initialize the first two column of Lp and all components of L. Seems to be correct

--- codes/project01_1d_model_cpp/Gauss.cpp
+++ codes/project01_1d_model_cpp/Gauss.cpp
@@ -37,16 +37,113 @@ int Gauss(int N, int a, int b)
         y[ii] = cos((2*lin[ii] + 1) * PI / (2 * N + 2)) + (0.27 / N1) * sin(PI * xu[ii] * N / N2);
     }
 
-     for (int ii = 0; ii < N1; ++ii)
+    for (int ii = 0; ii < N1; ++ii)
     {
         std::cout << "y[" << ii << "] = " << y[ii] << std::endl;
     }
 
-    return 0;
+    // Legendre-Gauss Vandermore Matrix
+    double L[N1 * N2];
+
+    for (int ii = 0; ii < N1; ++ii)
+    {
+        for (int jj = 0; jj < N2; ++jj)
+        {
+            L[ii * N2 + jj] = 0;
+        }
+    }
+
+    
+
+    // Derivative of LGVM
+    double Lp[N1 * N2];
+
+    for (int ii = 0; ii < N1; ++ii)
+    {
+        for (int jj = 0; jj < N2; ++jj)
+        {
+            Lp[ii * N2 + jj] = 0;
+        }
+    }
+
+    // for (int ii = 0; ii < N1 * N2; ++ii)
+    // {
+    //     std::cout << "Lp[" << ii << "] = " << Lp[ii] << std::endl;
+    // }
+    
+    // Compute the zeros of the N+1 Legendre Polynomial
+    // using the recursion relation and the Newton-Raphson method
+
+    int y0 = 2;
+
+    // Iterate until new points are uniformly within epsilon of old points
+    double max_value = 0.0;
+    for (int ii = 0; ii < N1; ++ii)
+    {
+        if (abs(y[ii] - y0) > max_value)
+        {
+            max_value = abs(y[ii] - y0);
+        }
+    }
+
+    // std::cout << "max_value = " << max_value << std::endl;
+ 
+
+    for (int kk = 1; kk < N1; ++kk)
+    {
+        for (int ii = 0; ii < N1; ++ii)
+        {
+            L[ii * N2 + (kk + 1)] =( (2 * (kk+1) - 1) * y[ii] * L[ii * N2 + kk] - kk * L[ii * N2 + (kk - 1)] ) / (kk + 1);
+        }
+    }
+
+
+    // for (int ii = 0; ii < N1 * N2; ++ii)
+    // {
+    //     std::cout << "L[" << ii << "] = " << L[ii] << std::endl;
+    // }
+
+
+    while (max_value > __DBL_EPSILON__)
+    {
+        for (int ii = 0; ii < N1; ++ii)
+        {
+            L[ii * N2] = 1;
+        }
+
+        for (int ii = 0; ii < N1; ++ii)
+        {
+            Lp[ii * N2] = 0;
+        }
+
+        for (int ii = 0; ii < N1; ++ii)
+        {
+            L[ii * N2 + 1] = y[ii];
+        }
+
+        for (int ii = 0; ii < N1; ++ii)
+        {
+            Lp[ii * N2 + 1] = 1;
+        }
+
+        for (int kk = 1; kk < N1; ++kk)
+        {
+            for (int ii = 0; ii < N1; ++ii)
+            {
+                L[ii * N2 + (kk + 1)] =( (2 * (kk+1) - 1) * y[ii] * L[ii * N2 + kk] - kk * L[ii * N2 + (kk - 1)] ) / (kk + 1);
+            }
+        }
+        
+
+        
+
+    }
+    
+
 
    
 
-    
+    return 0;
 
 }
 
