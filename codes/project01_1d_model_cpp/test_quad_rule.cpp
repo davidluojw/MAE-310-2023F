97834db63157a9282f9021317e64f3268f3c8b4e
lawkawai <ddlljiawei@163.com>
2023-12-05 11:40:00 +0800

    write test_quad_ruile.cpp , test it, seems good

---
+++ codes/project01_1d_model_cpp/test_quad_rule.cpp
@@ -0,0 +1,36 @@
+#include "Gauss.hpp"
+#include "typeinfo"
+
+int main()
+{
+    int n_int = 3;
+
+    GaussOutput * Gop = new GaussOutput;
+
+    Gop = Gauss(n_int, -1, 1);
+
+    int n = 6;
+
+    double exact;
+
+    exact = (1 - pow(-1, n+1)) / (n + 1);
+
+    double appro = 0.0;
+
+   
+
+    for (int ll = 0; ll < n_int; ++ll)
+    {
+        appro = appro + Gop->w[ll] * pow(Gop->x[ll], n);
+    }
+
+    double error;
+
+    error = exact - appro;
+
+    std::cout << "error = " << error << std::endl;
+
+    return 0;
+
+
+}
